0162ae2fce0296e8d39d612320ca39d693512aec
Nick Brown <nworbn@gmail.com>
2025-03-02 21:07:52 -0800

    starting pwm

--- firmware/src/bin/async_main.rs
+++ firmware/src/bin/async_main.rs
@@ -5,14 +5,37 @@ use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
 use esp_hal::clock::CpuClock;
+use esp_hal::ledc::{
+    channel,
+    timer::{self, TimerIFace},
+    LSGlobalClkSource, Ledc, LowSpeed,
+};
 use log::info;
 
 extern crate alloc;
 
+// DS18b20
+
+#[embassy_executor::task]
+async fn run() {
+    loop {
+        esp_println::println!("Hello world from embassy using esp-hal-async!");
+        Timer::after(Duration::from_millis(1_000)).await;
+    }
+}
+
+// loop {
+//     // Set up a breathing LED: fade from off to on over a second, then
+//     // from on back off over the next second.  Then loop.
+//     channel0.start_duty_fade(0, 100, 1000)?;
+//     while channel0.is_duty_fade_running() {}
+//     channel0.start_duty_fade(100, 0, 1000)?;
+//     while channel0.is_duty_fade_running() {}
+// }
+
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
-    // generator version: 0.2.2
-
+    // let peripherals = esp_hal::init(esp_hal::Config::default());
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
@@ -32,6 +55,24 @@ async fn main(spawner: Spawner) {
         peripherals.RADIO_CLK,
     )
     .unwrap();
+
+    let mut ledc = Ledc::new(peripherals.LEDC);
+    ledc.set_global_slow_clock(LSGlobalClkSource::APBClk);
+
+    let mut lstimer0 = ledc.timer::<LowSpeed>(timer::Number::Timer0);
+    lstimer0.configure(timer::config::Config {
+        duty: timer::config::Duty::Duty5Bit,
+        clock_source: timer::LSClockSource::APBClk,
+        frequency: Rate::from_khz(24),
+    })?;
+
+    let led = peripherals.GPIO2;
+    let mut channel0 = ledc.channel(channel::Number::Channel0, led);
+    // channel0.configure(channel::config::Config {
+    //     timer: &lstimer0,
+    //     duty_pct: 10,
+    //     pin_config: channel::config::PinConfig::PushPull,
+    // })?;
 
     // TODO: Spawn some tasks
     let _ = spawner;
